b3410aa1ec5ec6644bc263adb728bd8142225711
MswTester <mswtester10417@gmail.com>
2025-07-22 14:34:44 +0900

    need to fix it

--- apps/be/src/index.ts
+++ apps/be/src/index.ts
@@ -2,6 +2,7 @@ import Elysia from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import cors from "@elysiajs/cors";
 import { PrismaClient } from "@prisma/client";
+import cookie from "@elysiajs/cookie";
 
 const prisma = new PrismaClient();
 
@@ -14,6 +15,7 @@ prisma.$connect().then(() => {
 const PORT = process.env.PORT || 8000;
 
 const app = new Elysia()
+  // Plugins
   .use(cors({
     origin: process.env.ALLOWED_ORIGINS?.split(",") || ["http://localhost:3000", `http://localhost:${PORT}`],
     credentials: true,
@@ -21,6 +23,22 @@ const app = new Elysia()
   .use(swagger({
     path: "/swagger"
   }))
+  .use(cookie({
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 60 * 60 * 24 * 7
+  }))
+  // Routes
+  .get("/", ({redirect}) => redirect("/health"))
+  .get("/health", () => "OK")
+  // Error handling
+  .onError(({ error, set }) => {
+    console.error(error);
+    set.status = 500;
+    return "Internal Server Error";
+  })
+  // Start server
   .listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
 
 export default app;
